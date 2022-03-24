fd186001888633eefa41b12303f68c96e94f9fbc
Krzysztof Rozbicki <82657003+KrzysztofRoz@users.noreply.github.com>
2022-03-24 22:16:23 +0100

    Add method removeExcess in ShopFridge
    removeExcess method removes items from shopping list which excees max value or change the quantity of Products.

--- src/main/java/ShopFridge.java
+++ src/main/java/ShopFridge.java
@@ -99,6 +99,35 @@ class ShopFridge {
         }
         return list;
     }
+    
+    
+        public ShoppingList removeExcess(Fridge fridge, ShoppingList shoppingList){
+
+        LinkedList<FridgeProduct> fridgeContent=fridge.getContents();
+        LinkedList<Product> shoppingListContent=shoppingList.getShoppingList();
+
+    //remove products from shopping list if we got too much of the product in our fridge, or change the products quantity to not exceeds max value
+        for(int i=0;i<shoppingListContent.size();i++){
+            String productName=shoppingListContent.get(i).getName();
+            for(int j=0;j<fridgeContent.size();j++){
+                String fridgeProductName=fridgeContent.get(j).getName();
+                if(fridgeProductName.equals(productName)){
+                    if(fridgeContent.get(j).getMax()<
+                            fridgeContent.get(j).getQuantity()+shoppingListContent.get(i).getQuantity()){
+                        if(fridgeContent.get(j).getMax()<=fridgeContent.get(j).getQuantity()){
+                            shoppingListContent.remove(i);
+                        }else
+                        {
+                            shoppingListContent.get(i).setQuantity((fridgeContent.get(j).getMax()-fridgeContent.get(j).getQuantity()));
+                        }
+                    }
+                }
+            }
+        }
+        ShoppingList checkedShoppingList=new ShoppingList();
+        checkedShoppingList.addToShoppingList(shoppingListContent);
+        return checkedShoppingList;
+    }
 
 
 }
@@ -136,6 +165,9 @@ class Product{
         this.name=name;
         this.quantity=0;
     }
+    
+    public void setQuantity(int quantity){this.quantity=quantity;}
+
 
     public int getQuantity() {
         return quantity;
