1e84f209227ca403b1d8aaa7435a81e441c6b120
Krzysztof Rozbicki <82657003+KrzysztofRoz@users.noreply.github.com>
2022-03-30 10:29:41 +0200

    Update ShopFridge.java

--- src/main/java/ShopFridge.java
+++ src/main/java/ShopFridge.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.HashMap;
-import java.util.LinkedList;
+import java.util.ArrayList;
 
 class ShopFridge {
 
