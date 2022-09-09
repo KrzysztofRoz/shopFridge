c5474898afef9f2bbeb2280246eb3a2ce9100ae2
Krzysztof Rozbicki <82657003+KrzysztofRoz@users.noreply.github.com>
2022-09-09 12:51:02 +0200

    Add editing options
    Now you can change name of lists, fridges and products in it.
    Also adding posibility to change quantity of products and MinMax value of fridge products

--- src/main/java/EditView.java
+++ src/main/java/EditView.java
@@ -49,8 +49,9 @@ public class EditView {
                     Write:
                      1-to Change name\s
                      2-to Delete\s
-                     3-to Edit Products
-                     4-to Exit from edit mode""");
+                     3-to Add Products
+                     4-to Edit Product
+                     5-to Exit from edit mode""");
         try {
             Integer chosenOption=Integer.parseInt(reader.readLine());
             optionResolver(chosenOption);
@@ -69,32 +70,21 @@ public class EditView {
                 delete();
                 break;
             case(3):
+                if(isFridge){
+                    addFridgeProduct();
+                }
+                else{
+                    addProductToShoppingList();
+                }
                 break;
             case(4):
+                selectProduct();
+                break;
+            case (5):
                 break;
             default:
                 System.out.println("Write a number 1-4.");
                 editOption();
-        }
-
-    }
-    public void delete() throws IOException {
-        System.out.println("Are you sure you want delete this? Y/N" );
-        if(isFridge()){
-            this.showActiveFridge();
-        }else{
-            this.showActiveShoppingList();
-        }
-        String isYes= reader.readLine().toLowerCase(Locale.ROOT);
-
-        if(isYes.equals("y")||isYes.equals("yes")){
-            if(isFridge()) {
-                fridges.remove(activeFridge);
-                shopFridge.setMyFridges(fridges);
-            }else{
-                shoppingLists.remove(activeShoppingList);
-                shopFridge.setMyShoppingLists(shoppingLists);
-            }
         }
 
     }
@@ -120,13 +110,219 @@ public class EditView {
             shopFridge.setMyShoppingLists(shoppingLists);
 
         }
+    }
+    public void delete() throws IOException {
+        System.out.println("Are you sure you want delete this? Y/N" );
+        if(isFridge()){
+            this.showActiveFridge();
+        }else{
+            this.showActiveShoppingList();
+        }
+        String isYes= reader.readLine().toLowerCase(Locale.ROOT);
 
+        if(isYes.equals("y")||isYes.equals("yes")){
+            if(isFridge()) {
+                fridges.remove(activeFridge);
+                shopFridge.setMyFridges(fridges);
+            }else{
+                shoppingLists.remove(activeShoppingList);
+                shopFridge.setMyShoppingLists(shoppingLists);
+            }
+        }
+
+    }
+    public void addFridgeProduct(){
+        try {
+            ArrayList<FridgeProduct> newProducts=shopFridge.getCreator().createFridge();
+            fridges.get(fridges.indexOf(activeFridge)).addToFridge(newProducts);
+            shopFridge.setMyFridges(fridges);
+        } catch (IOException e) {
+            System.out.println("Enter the name, and correct quantity (non-negative number)");
+            addFridgeProduct();
+        }
+    }
+    public void addProductToShoppingList(){
+        try {
+            ArrayList<Product> newProducts=shopFridge.getCreator().createShoppingList();
+            shoppingLists.get(shoppingLists.indexOf(activeShoppingList)).addToShoppingList(newProducts);
+        } catch (IOException e ) {
+            System.out.println("Enter the name, and correct quantity (non-negative number)");
+            addFridgeProduct();
+        }
+    }
+
+ /// method to select product to edit
+    public void selectProduct() {
+        if(isFridge()){
+            this.showActiveFridge();
+            System.out.println("Write name of product you want change:");
+            try {
+                String selectedProductName= reader.readLine();
+                FridgeProduct selectedProduct=activeFridge.getContents().get(activeFridge.indexOfProduct(selectedProductName));
+                choseEditOptionFridgeProduct(selectedProduct);
+            } catch(IndexOutOfBoundsException e){
+                System.out.println("Cannot find product.");
+                selectProduct();
+            } catch (IOException e) {
+                System.out.println("Inappropriate name");
+                selectProduct();
+            }
+        }else{
+            this.showActiveShoppingList();
+            System.out.println("Write name of product you want change:");
+            try {
+                String selectedProductName= reader.readLine();
+                Product selectedProduct=activeShoppingList.getShoppingList().get(activeShoppingList.indexOfProduct(selectedProductName));
+                choseEditOptionProduct(selectedProduct);
+            } catch(IndexOutOfBoundsException e){
+                System.out.println("Cannot find product.");
+                selectProduct();
+            } catch (IOException e) {
+                System.out.println("Inappropriate name");
+                selectProduct();
+            }
+        }
+//Methods to edit products in shopping list change names and quantity
+
+    } public void choseEditOptionProduct(Product selectedProduct){
+        System.out.println(selectedProduct);
+        System.out.println("""
+
+                    What you want edit
+                    Write:
+                     1-to Change product name\s
+                     2-to Change product quantity\s
+                     3-to Remove product from Shopping List\s 
+                     4-to Exit from edit mode""");
+        try {
+            Integer option=Integer.parseInt(reader.readLine());
+            switch(option){
+                case(1):
+                    changeProductName(selectedProduct);
+                    break;
+                case (2):
+                    changeProductQuantity(selectedProduct);
+                    break;
+                case (3):
+                    listProductRemove(selectedProduct);
+                    break;
+                case (4):
+                    break;
+                default:
+                    System.out.println("Write option number 1-4");
+                    choseEditOptionProduct(selectedProduct);
+            }
+        } catch(NullPointerException | IOException e){
+            System.out.println("Please chose a option 1-4 ");
+            choseEditOptionProduct(selectedProduct);
+        }
+
+    }
+    public void changeProductName(Product selectedProduct){
+        System.out.println("Write new product name:");
+        try {
+            String newName= reader.readLine();
+            selectedProduct.setProduct(newName, selectedProduct.getQuantity());
+        } catch (IOException e) {
+            System.out.println("Insert new name.");
+            changeProductName(selectedProduct);
+        }
+    }
+    public void changeProductQuantity(Product selectedProduct){
+        System.out.println("Write new quantity of " + selectedProduct.getName() +" (must be numerical value):");
+        try {
+            Integer newQuantity= Integer.parseInt(reader.readLine());
+            selectedProduct.setQuantity(newQuantity);
+        } catch (IOException e) {
+            System.out.println("Insert quantity.");
+            changeProductQuantity(selectedProduct);
+        }
+    }
+    public void listProductRemove(Product selectedProduct){
+        shoppingLists.get(shoppingLists.indexOf(activeShoppingList)).remove(selectedProduct);
+        shopFridge.setMyShoppingLists(shoppingLists);
+    }
+
+//Methods to edit fridge products change names,quantity and Min/Max settings
+    public void choseEditOptionFridgeProduct(FridgeProduct selectedProduct){
+        System.out.println(selectedProduct);
+        System.out.println("""
+
+                    What you want edit
+                    Write:
+                     1-to Change product name\s
+                     2-to Change product quantity\s
+                     3-to Change min and max
+                     4-to Remove product from fridge 
+                     5-to Exit from edit mode""");
+        try {
+            Integer option=Integer.parseInt(reader.readLine());
+            switch(option){
+                case(1):
+                    changeFridgeProductName(selectedProduct);
+                    break;
+                case (2):
+                    changeFridgeProductQuantity(selectedProduct);
+                    break;
+                case (3):
+                    changeMinMax(selectedProduct);
+                    break;
+                case (4):
+                    fridgeProductRemove(selectedProduct);
+                    break;
+                case (5):
+                    break;
+                default:
+                    System.out.println("Write option number 1-5");
+                    choseEditOptionFridgeProduct(selectedProduct);
+            }
+        } catch(NullPointerException | IOException e){
+            System.out.println("Please chose a option 1-5 ");
+            choseEditOptionFridgeProduct(selectedProduct);
+        }
 
     }
 
-    public void nameChanger(String newName){
+    ///check if i can use this method after change FridgeProduct to Product by both Fridge and shoppingList
+    public void changeFridgeProductName(FridgeProduct selectedProduct){
+        System.out.println("Write new product name:");
+        try {
+            String newName= reader.readLine();
+            selectedProduct.setProduct(newName, selectedProduct.getQuantity());
+        } catch (IOException e) {
+            System.out.println("Insert new name.");
+            changeFridgeProductName(selectedProduct);
+        }
+    }
+    public void changeFridgeProductQuantity(FridgeProduct selectedProduct){
+        System.out.println("Write new quantity of " + selectedProduct.getName() +" (must be numerical value):");
+        try {
+            Integer newQuantity= Integer.parseInt(reader.readLine());
+            selectedProduct.setQuantity(newQuantity);
+        } catch (IOException e) {
+            System.out.println("Insert quantity.");
+            changeFridgeProductQuantity(selectedProduct);
+        }
+    }
+    public void changeMinMax(FridgeProduct selectedProduct){
+        System.out.println("Write new minimal quantity of " + selectedProduct.getName() +" (must be non-negative numerical value):");
+        try {
+            Integer newMin= Integer.parseInt(reader.readLine());
+            selectedProduct.setMin(newMin);
+            System.out.println("Write new maximal quantity of " + selectedProduct.getName() +" (must be non-negative numerical value, greater than min value):");
+            selectedProduct.setMax(Integer.parseInt(reader.readLine()));
+        } catch (IOException e) {
+            System.out.println("Insert correct value. Minimal value must be non-negative number. Maximal value must be greater than minimal");
+            changeMinMax(selectedProduct);
+        }
 
     }
+    public void fridgeProductRemove(FridgeProduct selectedProduct){
+        fridges.get(fridges.indexOf(activeFridge)).remove(selectedProduct);
+        shopFridge.setMyFridges(fridges);
+    }
+
+
 
 
 
